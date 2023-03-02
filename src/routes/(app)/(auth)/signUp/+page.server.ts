e40818b2038bc1102ec3b437f47034226c3d9164
Chris Wilson <cpwilson87@outlook.com>
2023-03-02 08:46:26 +0000

    feat: add zod schema validation and build getFormData util function

--- src/routes/(app)/(auth)/signUp/+page.server.ts
+++ src/routes/(app)/(auth)/signUp/+page.server.ts
@@ -1,9 +1,48 @@
-import type { Actions, PageServerLoad } from './$types';
+import type { Actions, PageServerLoad, RequestEvent } from './$types';
+import { z } from 'zod';
+
+const signUpSchema = z
+	.object({
+		email: z
+			.string({ required_error: 'Email is required' })
+			.min(1, { message: 'Email is required' })
+			.max(64, { message: 'Email must be less than 64 characters' })
+			.email({ message: 'Email must be a valid email address' }),
+		password: z
+			.string({ required_error: 'Password Required' })
+			.min(6, { message: 'Password must be at least 6 characters' }),
+		confirmPassword: z
+			.string({ required_error: 'Password Required' })
+			.min(6, { message: 'Password must be at least 6 characters' })
+	})
+	.superRefine(({ password, confirmPassword }, ctx) => {
+		if (password !== confirmPassword) {
+			ctx.addIssue({
+				code: 'custom',
+				message: 'Password and Confirm Password must muatch',
+				path: ['password']
+			});
+			ctx.addIssue({
+				code: 'custom',
+				message: 'Password and Confirm Password must muatch',
+				path: ['confirmPassword']
+			});
+		}
+	});
+
+// Type
+type User = {
+	email: string;
+	password: string;
+	confirmPassword: string;
+};
 
 // Loader
 export const load: PageServerLoad = () => {
-	const user = {
-		email: 'test@test.com'
+	const user: User = {
+		email: 'test@test.com',
+		password: '123456',
+		confirmPassword: '123456'
 	};
 
 	return { user };
@@ -12,7 +51,31 @@ export const load: PageServerLoad = () => {
 // Actions
 export const actions: Actions = {
 	signUp: async (event) => {
-		console.log({ event });
+		const { formData, errors } = await getFormData(event);
+
+		if (errors) {
+			const fieldErrors = errors?.fieldErrors;
+			const formErrors = errors?.formErrors;
+			console.log({ fieldErrors, formErrors });
+			return;
+		}
+		console.log({ formData });
 		return;
 	}
 };
+
+// Validation
+async function getFormData(event: RequestEvent) {
+	const formData = Object.fromEntries(await event.request.formData());
+	const validation = signUpSchema.safeParse(formData);
+
+	let errors: z.typeToFlattenedError<User> | undefined;
+
+	if (!validation.success) {
+		errors = validation.error.flatten();
+		return { formData, errors };
+	}
+	return { formData, errors };
+}
+
+// NEXT STEP: Pass zod schema to getFormData
--- src/routes/(app)/(auth)/signUp/+page.svelte
+++ src/routes/(app)/(auth)/signUp/+page.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	let user: string;
+	let user: String = data?.user?.email ?? '';
+	let password: String = data?.user?.password ?? '';
+	let confirmPassword: String = data?.user?.confirmPassword ?? '';
 
-	user = data?.user?.email ?? '';
-
-	console.log({ data, user });
+	// console.log({ user });
 </script>
 
 <h1>Sign Up Form</h1>
 
-<form action="?/signUp" method="POST">
+<form action="?/signUp" method="POST" use:enhance>
 	<label for="email">Email</label>
 	<input type="text" name="email" id="email" value={user} />
 
 	<label for="password">Password</label>
-	<input type="password" name="password" id="password" />
+	<input type="password" name="password" id="password" value={password} />
 
 	<label for="confirmPassword">Confirm Password</label>
-	<input type="password" name="confirmPassword" id="confirmPassword" />
+	<input type="password" name="confirmPassword" id="confirmPassword" value={confirmPassword} />
 
 	<button type="submit">Create Account</button>
 </form>
